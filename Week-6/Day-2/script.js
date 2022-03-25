5589aa8dea8e683197bf365e373890fae8d08417
iqbalnova <iqbalnova707@gmail.com>
2022-03-25 21:17:04 +0700

    adding challenge 3

---
+++ Week-6/Day-2/script.js
@@ -0,0 +1,26 @@
+console.log('JSON')
+
+// JavaScript Object Notation
+
+const obj = {
+    name : "iqbal"
+}
+const stringyObj = JSON.stringify(obj)
+console.log(stringyObj)
+console.log(JSON.parse(stringyObj))
+
+// kriteria JSON
+// key dan value dibungkus string
+
+// Authorization
+// flow  untuk mengakses aplikasi
+
+// Authentication
+// Proses user diberi akses
+
+// Authenticated
+// user yang sudah terautentikasi
+
+
+// membuat loading dengan web RN Lottie / spinner
+// shimmer untuk skeleton mode
